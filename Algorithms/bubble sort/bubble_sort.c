6ba9d3c6aac5b7f8c816d1e7971a6dde89dade2a
sumitisnot4u <56196142+sumitisnot4u@users.noreply.github.com>
2020-10-01 08:54:02 +0530

    Update bubble_sort.c

--- Algorithms/bubble sort/bubble_sort.c
+++ Algorithms/bubble sort/bubble_sort.c
@@ -1,77 +1,41 @@
-#include <stdio.h>
-
-
-
-
-
-
-
-
-
-
-void bubble_sort(int array[], int size)
-{
-    // hold values for swaping
-    int hold;
-
-
-
-    for (int i = 0; i < size; i++)
-        for (int j = i+1; j < size; j++)
-            if (array[i] > array[j])
-            {
-                hold = array[i];
-                array[i] = array[j];
-                array[j] = hold;
-            }
-
-
-    return;
-}
-
-
-
-
-
-// a simple function for printing arrays 
-void printArray(int array[], int size)
-{
-    printf("[");
-
-    for (int i = 0; i < size; i++)
-        printf("%d, ", array[i]);
-
-    printf("\b\b]\n");
-}
-
-
-
-
-
-
-int main()
-{
-    printf("\n\n ---------------- \n\n");
-    printf("a simple test:\n\n");
-    
-
-    int array[] = {2, 12, 4, 0, -21, 23, -11, 43, 432, 130, 99, 33};
-
-    printf("before sort: ");
-    printArray(array, 12);
-
-
-
-    bubble_sort(array, 12);
-
-    printf("after sort: ");
-    printArray(array, 12);
-
-
-    printf("\n ---------------- \n\n\n");
-
-
-
-
-    return 0;
-}
+// C program for implementation of Bubble sort 
+#include <stdio.h> 
+  
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+// A function to implement bubble sort 
+void bubbleSort(int arr[], int n) 
+{ 
+   int i, j; 
+   for (i = 0; i < n-1; i++)       
+  
+       // Last i elements are already in place    
+       for (j = 0; j < n-i-1; j++)  
+           if (arr[j] > arr[j+1]) 
+              swap(&arr[j], &arr[j+1]); 
+} 
+  
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("\n"); 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubbleSort(arr, n); 
+    printf("Sorted array: \n"); 
+    printArray(arr, n); 
+    return 0; 
+} 
