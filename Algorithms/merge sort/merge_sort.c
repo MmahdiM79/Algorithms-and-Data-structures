4d6a5c5687526d4b27e1ae16154fe5b96801b977
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-10-01 11:26:58 +0330

    minor changes

--- Algorithms/merge sort/merge_sort.c
+++ Algorithms/merge sort/merge_sort.c
@@ -13,31 +13,31 @@ void merge(int array[], int startIndex, int halfIndex, int lastIndex)
 
 
     // initialize first sub array
-    int firstArray[firstSubArraySize + 1];
+    int rightArray[firstSubArraySize + 1];
     for (int i = 0; i < firstSubArraySize; i++)
-        firstArray[i] = array[startIndex + i - 1];
+        rightArray[i] = array[startIndex + i - 1];
 
 
     // initialize second sub array
-    int secondArray[secondSubArraySize + 1];
+    int leftArray[secondSubArraySize + 1];
     for (int j = 0; j < secondSubArraySize; j++)
-        secondArray[j] = array[halfIndex + j];
+        leftArray[j] = array[halfIndex + j];
 
 
     
 
     // set sentinels
-    firstArray[firstSubArraySize+1] = __INT_MAX__;
-    secondArray[secondSubArraySize+1] = __INT_MAX__;
+    rightArray[firstSubArraySize+1] = __INT_MAX__;
+    leftArray[secondSubArraySize+1] = __INT_MAX__;
 
 
 
     // merging
     int i = 1, j = 1;
     for (int k = startIndex; k < lastIndex; k++)
-        if (firstArray[i] <= secondArray[j])
-            array[k] = firstArray[i++];
+        if (rightArray[i] <= leftArray[j])
+            array[k] = rightArray[i++];
         else
-            array[k] = secondArray[j++];
+            array[k] = leftArray[j++];
 
 }
