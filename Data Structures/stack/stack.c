8ce2feb686244a243b1e7c8d9af0e48d52bd1643
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-10-16 16:01:09 +0330

    implementaion of stack data structure in C added

---
+++ Data Structures/stack/stack.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#define error(x) {printf("\n\n"#x"\n"); exit(-1);}
+#define nl printf("\n")
+
+
+
+
+
+
+
+
+/*      stack struct        */
+
+typedef struct __STACK__
+{
+    int arr[1000];
+    int top;
+}stack;
+
+#define newStackMalloc() (stack *) malloc(sizeof(stack))
+#define forEach(s, expression) for (int i = 0; i <= s.top; i++) {expression}
+
+
+
+
+
+
+/*      functions       */
+
+bool isStackEmpty(stack s)
+{
+    return (s.top == -1);
+}
+
+
+void push(stack *s, int value)
+{
+    s->arr[++(s->top)] = value;
+}
+
+
+int pop(stack *s)
+{
+    if (isStackEmpty(*s))
+        error(underflow);
+
+    return s->arr[(s->top)--];
+}
+
+
+void printStack(stack s)
+{
+    printf("stack:  ");
+    forEach(s, printf("%d, ", s.arr[i]););
+    printf("\b\b\b\n");
+}
+
+
+
+
+
+
+
+int main()
+{
+    printf("\n\n ---------------- \n\n");
+    printf("a simple test:\n\n");
+
+    
+    // initialize a new stack
+    stack *s = newStackMalloc();
+    s->top = -1;
+
+    // add some values
+    push(s, 1);
+    push(s, 2);
+    push(s, 3);
+    push(s, 4);
+    push(s, 5);
+    push(s, 6);
+    push(s, 7);
+    push(s, 8);
+    push(s, 9);
+    push(s, 10);
+    push(s, 11);
+    push(s, 12);
+
+    
+    printStack(*s);
+    nl;
+
+
+    push(s, 14);
+    printf("after push value 14:  \n"); 
+    printStack(*s);
+    nl;
+
+    
+    
+
+    printf("call pop function:  %d\n", pop(s));
+    nl;
+
+
+    printf("after pop value 14:  "); printStack(*s);
+    nl;
+
+
+
+
+    printf("\n\n ---------------- \n\n\n");
+
+    return 0;
+}
