36018638ea5b0186f7045347ed029ea4be8c3d39
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-09 13:50:55 +0330

    new functions added

--- Data Structures/binary search tree/binary_search_tree.c
+++ Data Structures/binary search tree/binary_search_tree.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 
 
@@ -15,14 +17,49 @@ typedef struct __NODE__
 
 } node;
 
+#define Node node *
+
+node *new_node(int value)
+{
+    node *output = (node *)malloc(sizeof(node));
+
+    output->value = value;
+    output->p = output->right = output->left = NULL;
+
+    return output;
+}
+
+
+
+
 
 
 typedef struct __BINARY_SEARCH_TREE__
 {
     node *root;
     int size;
-    
+
 } BST;
+
+#define Bst BST *
+
+BST *new_bst()
+{
+    BST *output = (BST *)malloc(sizeof(BST));
+
+    output->root = NULL;
+    output->size = 0;
+
+    return output;
+}
+
+
+
+
+
+
+
+
 
 
 
