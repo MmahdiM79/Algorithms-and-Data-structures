7776b78573849fb6ba3376366516c92020f72fa0
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-13 15:29:57 +0330

    transplant method added

--- Data Structures/binary search tree/binary_search_tree.py
+++ Data Structures/binary search tree/binary_search_tree.py
@@ -4,25 +4,31 @@
 class BinarySearchTree(object):
     
     class Node(object):
+        
         def __init__(self, value: int) -> None:
             self.value = value
             self.p = None
             self.right = None
             self.left = None
             
+         
+         
             
             
     def __init__(self) -> None:
+        
         self.__root__ = None
         self.__size__ = 0
         
     
+    
     def insert(self, value: int) -> None:
+        
         new_node = self.Node(value)
         current = self.__root__
         hold = None
         
-        while current != None:
+        while current is not None:
             hold = current
             
             if new_node.value < current.value:
@@ -33,7 +39,7 @@ class BinarySearchTree(object):
                 
         new_node.p = hold
         
-        if hold == None:
+        if hold is None:
             self.__root__ = new_node
         
         elif new_node.value < hold.value:
@@ -44,6 +50,25 @@ class BinarySearchTree(object):
             
             
         self.__size__ += 1
+        
+        
+        
+    def __transplant__(self, node1: Node, node2: Node) -> None:
+        
+        if node1.p is None:
+            self.__root__ = node2
+            
+        elif node1 is node1.p.left:
+            node1.p.left = node2
+            
+        else:
+            node1.p.right = node2
+            
+        
+        if node2.p is not None:
+            node2.p = node1.p
+            
+        
     
     
     
