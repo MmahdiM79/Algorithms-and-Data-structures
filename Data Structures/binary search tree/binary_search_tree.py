930b1cfa1ee78698107e1a4d7b004119cf59b6d4
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-13 15:19:49 +0330

    insert method added

--- Data Structures/binary search tree/binary_search_tree.py
+++ Data Structures/binary search tree/binary_search_tree.py
@@ -11,11 +11,39 @@ class BinarySearchTree(object):
             self.left = None
             
             
+            
     def __init__(self) -> None:
         self.__root__ = None
         self.__size__ = 0
         
     
+    def insert(self, value: int) -> None:
+        new_node = self.Node(value)
+        current = self.__root__
+        hold = None
+        
+        while current != None:
+            hold = current
+            
+            if new_node.value < current.value:
+                current = current.left
+            else:
+                current = current.right
+            
+                
+        new_node.p = hold
+        
+        if hold == None:
+            self.__root__ = new_node
+        
+        elif new_node.value < hold.value:
+            hold.left = new_node
+        
+        else:
+            hold.right = new_node
+            
+            
+        self.__size__ += 1
     
     
     
