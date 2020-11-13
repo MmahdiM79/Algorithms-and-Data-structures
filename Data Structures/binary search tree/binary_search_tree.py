086500c152a95685528fa577b395c8477b2e183b
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-13 15:06:47 +0330

    binary search tree Python file added

---
+++ Data Structures/binary search tree/binary_search_tree.py
@@ -0,0 +1,32 @@
+
+
+
+class BinarySearchTree(object):
+    
+    class Node(object):
+        def __init__(self, value: int) -> None:
+            self.value = value
+            self.p = None
+            self.right = None
+            self.left = None
+            
+            
+    def __init__(self) -> None:
+        self.__root__ = None
+        self.__size__ = 0
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    pass
+
+
