222a7e81db09efe332e327c3268e157681f40fe2
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-16 10:38:56 +0330

    insert method added

--- Data Structures/binary search tree/BinarySearchTree.java
+++ Data Structures/binary search tree/BinarySearchTree.java
@@ -1,6 +1,11 @@
 
 
-
+/**
+ * This class represent a simple binary search tree
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.1.0
+ */
 public class BinarySearchTree
 {
     private class Node
@@ -29,10 +34,54 @@ public class BinarySearchTree
 
 
          /* Constructor */
-
+    /**
+     * Create a new binary search tree
+     */
     public BinarySearchTree()
     {
         root = null;
         size = 0;
+    }
+
+
+            /*  Methods  */
+
+    /**
+     * insert a new node to the tree
+     * 
+     * @param value : value of new node
+     * @return the added node object
+     */
+    public Node insert(int value)
+    {
+        Node newNode = new Node(value);
+        
+        Node current = root;
+        Node hold = null;
+
+        while (current != null)
+        {
+            hold = current;
+
+            if (newNode.value < current.value)
+                current = current.left;
+            else
+                current = current.right;
+        }
+
+        newNode.p = hold;
+
+        if (hold == null)
+            root = newNode;
+
+        else if (newNode.value < hold.value)
+            hold.left = newNode;
+
+        else
+            hold.right = newNode;
+
+
+        size++;
+        return newNode;
     }
 }
