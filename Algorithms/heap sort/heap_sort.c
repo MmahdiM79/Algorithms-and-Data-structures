d76a9b2d47517af99a03d0041e421ca4b4a1de41
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-21 14:27:54 +0330

    __HEAP__ sturct added

--- Algorithms/heap sort/heap_sort.c
+++ Algorithms/heap sort/heap_sort.c
@@ -5,6 +5,26 @@
 
 
 
+typedef struct __HEAP__
+{
+    int *array;
+    long length;
+    long size;
+} HEAP;
+
+HEAP *new_heap(long length)
+{
+    HEAP *output = (HEAP *)malloc(sizeof(HEAP));
+
+    output->array = (int *)malloc(length * sizeof(int));
+    output->length = length;
+    output->size = 0;
+
+    return output;
+}
+
+
+
 #define PARENT(x) (x/2)
 #define RIGHT(x) (2*x + 1)
 #define LEFT(x) (2*x)
