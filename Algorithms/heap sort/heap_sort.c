ae4b12b5720ae9dcd6a71df2f2901e94f84bfd8d
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-23 14:08:28 +0330

    printArray function added

--- Algorithms/heap sort/heap_sort.c
+++ Algorithms/heap sort/heap_sort.c
@@ -96,6 +96,18 @@ void heap_sort(int array[])
 }
 
 
+// a simple function for printing arrays 
+void printArray(int array[], int size)
+{
+    printf("[");
+
+    for (int i = 0; i < size; i++)
+        printf("%d, ", array[i]);
+
+    printf("\b\b]\n");
+}
+
+
 
 
 
