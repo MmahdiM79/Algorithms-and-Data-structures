ea52365c4b98af1f23735df9ede4a5cd9d7c70ca
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-29 13:51:27 +0330

    swap micro added

--- Algorithms/heap sort/heap_sort.c
+++ Algorithms/heap sort/heap_sort.c
@@ -6,8 +6,17 @@
 
 
 
+#define swap(x, y) \
+        {\
+            int hold = x;\
+            x = y;\
+            y = hold;\
+        }
+
 #define RIGHT(x) (2*x + 2)
 #define LEFT(x) (2*x + 1)
+
+
 
 
 
