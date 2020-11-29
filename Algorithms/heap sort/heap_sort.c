e45ad3a03a1d7c032f28d21d9c5244ea4947bd1f
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-29 13:53:12 +0330

    max_heapfiy function added

--- Algorithms/heap sort/heap_sort.c
+++ Algorithms/heap sort/heap_sort.c
@@ -6,17 +6,40 @@
 
 
 
+
 #define swap(x, y) \
-        {\
             int hold = x;\
             x = y;\
             y = hold;\
-        }
+
 
 #define RIGHT(x) (2*x + 2)
 #define LEFT(x) (2*x + 1)
 
 
+
+void max_heapify(int array[], int size, int i)
+{
+    int l = LEFT(i);
+    int r = RIGHT(i); 
+ 
+
+    int largest;
+    if (l < size && array[l] > array[i])
+        largest = l;
+    else
+        largest = i;
+ 
+    if (r < size && array[r] > array[largest])
+        largest = r;
+
+ 
+    if (largest != i) 
+    {
+        swap(array[i], array[largest]);
+        max_heapify(array, size, largest);
+    }
+}
 
 
 
