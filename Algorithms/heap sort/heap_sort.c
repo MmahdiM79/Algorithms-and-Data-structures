f9a48c19ec43e26f29d5ad0bd80cfa8d1e744270
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-11-30 14:06:51 +0330

    example added

--- Algorithms/heap sort/heap_sort.c
+++ Algorithms/heap sort/heap_sort.c
@@ -69,7 +69,31 @@ void printArray(int array[], int size)
 
 
 
+
+
 int main()
 {
+    printf("\n\n ---------------- \n\n");
+    printf("a simple test:\n\n");
+    
+
+    int array[] = {2, 12, 4, 0, -21, 23, -11, 43, 432, 130, 99, 33};
+
+    printf("before sort: ");
+    printArray(array, 12);
+
+
+
+    heap_sort(array, 12);
+
+    printf("after sort: ");
+    printArray(array, 12);
+
+
+    printf("\n ---------------- \n\n\n");
+
+
+
+
     return 0;
 }
