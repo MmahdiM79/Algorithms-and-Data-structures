fc776c4e29ef5afc290e06c42d0fefbf6cc59143
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-12-05 10:29:47 +0330

    max_heapify function added

--- Algorithms/heap sort/heap_sort.py
+++ Algorithms/heap sort/heap_sort.py
@@ -12,6 +12,26 @@ def LEFT(x :int) -> int:
 
 
 
+def max_heapify(array :list, i :int) -> None:
+
+    l = LEFT(i)
+    r = RIGHT(r)
+
+
+    if l < len(array) and array[l] > array[i]:
+        largest = l
+    else:
+        largest = i
+
+    if r < len(array) and array[r] > array[largest]:
+        largest = r
+
+
+    if largest != i:
+        array[i], array[largest] = array[largest], array[i]
+        max_heapify(array, largest)
+    
+
 
 
 
