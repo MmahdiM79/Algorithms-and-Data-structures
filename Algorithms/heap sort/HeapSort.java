b1cdb8e38765b2c9ca3db284b3f96ebb62e65366
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-12-08 14:07:12 +0330

    some comments added

--- Algorithms/heap sort/HeapSort.java
+++ Algorithms/heap sort/HeapSort.java
@@ -10,11 +10,13 @@
  */
 public class HeapSort 
 {
+    // return the index of the right child of the given index
     private int right(int x)
     {
         return 2*x + 2;
     }
 
+    // return the index of the left child of the given index
     private int left(int x)
     {
         return right(x) - 1;
