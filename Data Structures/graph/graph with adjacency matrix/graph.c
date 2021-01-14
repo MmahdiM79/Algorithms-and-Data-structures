5b88d089315ae291039f57f16bbe4bcf2a3117d5
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-01-14 13:48:04 +0330

    add_edge function added

--- Data Structures/graph/graph with adjacency matrix/graph.c
+++ Data Structures/graph/graph with adjacency matrix/graph.c
@@ -31,11 +31,24 @@ GRAPH *new_graph(long number_of_V)
     out->number_of_E = 0;
 
     out->adj = (int **)malloc(number_of_V*sizeof(number_of_V*sizeof(long)));
+    for (int i = 0; i < number_of_V; i++)
+        for (int j = 0; j < number_of_V; j++)
+            out->adj[i][j] = -1;
 }
+
+
+
+
+
+        /*      Functions       */
+
 
 void add_edge(GRAPH *g, int u, int v)
 {
+    check_V(g, u);
+    check_V(g, v);
 
+    g->adj[u][v] = 1;
 }
 
 
