fbb422c600319f4648843c6926cd848c01d35578
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-01-21 22:05:58 +0330

    Graph class added

--- Data Structures/graph/graph with adjacency matrix/graph.py
+++ Data Structures/graph/graph with adjacency matrix/graph.py
@@ -3,6 +3,14 @@
 
 
 
+class Graph(object):
+
+
+
+    def __init__(self, number_of_V: int) -> None:
+        self.number_of_V = number_of_V
+        self.number_of_E = 0
+        self.adj = [[-1 for _ in range(number_of_V)] for _ in range(number_of_V)]
 
 
 
