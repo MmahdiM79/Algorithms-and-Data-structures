859828ee4d2b707808af6ed5c6a2a8877b937bed
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-12 11:01:34 +0330

    vertice class added

--- Data Structures/graph/graph with adjacency matrix/graph.py
+++ Data Structures/graph/graph with adjacency matrix/graph.py
@@ -3,9 +3,22 @@
 
 
 
+class Vertice(object):
+
+    def __init__(self, tag) -> None:
+        self.__tag = tag
+        self.__fields = {}
+
+
+    def add_field(self, **fields):
+        for key in fields.keys():
+            self.__fields[key] = fields[key]
+
+
+
+
+
 class Graph(object):
-
-
 
     def __init__(self, number_of_V: int) -> None:
         self.number_of_V = number_of_V
