c7ece9cd5b39016321e1b471d0378bbab439e095
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-12 11:22:24 +0330

    Graph init method updated

--- Data Structures/graph/graph with adjacency matrix/graph.py
+++ Data Structures/graph/graph with adjacency matrix/graph.py
@@ -28,9 +28,11 @@ class Vertice(object):
 class Graph(object):
 
     def __init__(self, number_of_V: int) -> None:
-        self.number_of_V = number_of_V
-        self.number_of_E = 0
-        self.adj = [[-1 for _ in range(number_of_V)] for _ in range(number_of_V)]
+        self.__number_of_V = number_of_V
+        self.__number_of_E = 0
+        self.__Vs = []
+        self.__Es = []
+        self.__adj = [[-1 for _ in range(number_of_V)] for _ in range(number_of_V)]
 
 
 
