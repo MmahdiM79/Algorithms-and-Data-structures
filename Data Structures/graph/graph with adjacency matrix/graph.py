8b2f06bc63e74284d0ae5ec2c789d7b0607989a6
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-12 11:24:45 +0330

    vertice class removed

--- Data Structures/graph/graph with adjacency matrix/graph.py
+++ Data Structures/graph/graph with adjacency matrix/graph.py
@@ -1,39 +1,14 @@
 
 
 
-
-
-from typing import Any
-
-
-class Vertice(object):
-
-    def __init__(self, tag: Any) -> None:
-        self.__tag = tag
-        self.__fields = {}
-
-
-    def add_field(self, **fields) -> None:
-        for key in fields.keys():
-            self.__fields[key] = fields[key]
-
-
-    def tag(self) -> Any:
-        return self.__tag
-
-
-
-
-
 class Graph(object):
 
     def __init__(self, number_of_V: int) -> None:
         self.__number_of_V = number_of_V
         self.__number_of_E = 0
-        self.__Vs = []
-        self.__Es = []
+        self.__Vs = {}
+        self.__Es = {}
         self.__adj = [[-1 for _ in range(number_of_V)] for _ in range(number_of_V)]
-
 
 
 
