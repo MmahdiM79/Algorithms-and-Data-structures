edeee7568c52fb240f08c0c788d9682ad1c498f4
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-14 09:52:48 +0330

    printArray function added

--- Algorithms/radix sort/radix_sort.c
+++ Algorithms/radix sort/radix_sort.c
@@ -7,6 +7,21 @@
 
 
 
+
+// a simple function for printing arrays 
+void printArray(int array[], int size)
+{
+    printf("[");
+
+    for (int i = 0; i < size; i++)
+        printf("%d, ", array[i]);
+
+    printf("\b\b]\n");
+}
+
+
+
+
 int main()
 {
     return 0;
