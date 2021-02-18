76c599fc476f8af0b755ef5758f39b190b12b6df
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-18 10:03:34 +0330

    counting_sort function added

--- Algorithms/radix sort/radix_sort.c
+++ Algorithms/radix sort/radix_sort.c
@@ -13,6 +13,31 @@
 
 
 
+int *counting_sort(int[] array, int n, int exp)
+{
+    int count[10+!] = {0};
+
+
+    for (int i = 0; i < n; i++)
+        count[index(array[i], exp)] += 1;
+
+    for (int j = 1; j < 10+1; j++)
+        count[j] += count[i-1];
+
+    
+    int out[n];
+    for (int k = n-1; k >= 0; k--)
+    {
+        out[count[index(array[k], exp)]-1] = array[k];
+        count[index(array[k], exp)] -= 1;
+    }
+
+
+    return out;
+}
+
+
+
 
 // a simple function for printing arrays 
 void printArray(int array[], int size)
