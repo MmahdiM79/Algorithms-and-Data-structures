8b92a9e559b083b2bde5e0e87c2a3b1d25be5119
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-03-05 15:55:22 +0330

    max_array function updated

--- Algorithms/radix sort/radix_sort.c
+++ Algorithms/radix sort/radix_sort.c
@@ -9,10 +9,10 @@
 
 
 
-int max_array(int array[])
+int max_array(int array[], int size)
 {
     int max = array[0];
-    for (int i = 0; i < sizeof(array)/sizeof(int); i++)
+    for (int i = 0; i < size; i++)
         if (array[i] > max)
             max = array[i];
 
