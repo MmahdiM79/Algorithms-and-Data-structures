dcdca314a23ec5aa606ebc61c77ae26a734f3402
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-04-27 15:52:09 +0430

    counting_sort function added

--- Algorithms/counting sort/counting_sort.py
+++ Algorithms/counting sort/counting_sort.py
@@ -9,7 +9,23 @@
 
 
 
+def counting_sort(array: list, n: int, k: int) -> list:
 
+    count = [0] * (k+1)
+
+    for j in range(n):
+        count[array[j]] += 1
+
+    for i in range(1, k+1):
+        count[i] += count[i-1]
+
+    out = [None] * n
+    for j in range(n-1, -1, -1):
+        out[count[array[j]]-1] = array[j]
+        count[array[j]] -= 1
+
+
+    return out
 
 
 
@@ -17,4 +33,3 @@
 
 
 if __name__ == "__main__":
-    
